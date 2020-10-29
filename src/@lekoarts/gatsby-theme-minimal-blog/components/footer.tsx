108c44266323580bc07eb93fab1d58ed2a267c71
Nick Yeager <nick@quoteapro.com>
2020-10-29 06:17:04 -0700

    add custom components, including the header and footer components

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui"
+import useSiteMetadata from "../hooks/use-site-metadata"
+
+const Footer = () => {
+    const { siteTitle } = useSiteMetadata()
+
+    return (
+        <footer
+            sx={{
+                boxSizing: `border-box`,
+                display: `flex`,
+                justifyContent: `space-between`,
+                mt: [6],
+                color: `secondary`,
+                a: {
+                    variant: `links.secondary`,
+                },
+                flexDirection: [`column`, `column`, `row`],
+                variant: `dividers.top`,
+            }}
+        >
+            <div>
+                &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
+            </div>
+            <div>
+                <Link aria-label="Link to Nicks site" href="https://www.tucsonaz.gov/">
+                    Made with love in Tucson
+                </Link>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
